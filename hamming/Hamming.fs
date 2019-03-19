9767d09dea6719fd734a6cf89b26338880512298
Oguzhan Ergin <oguzhanergin13@gmail.com>
2019-03-19 21:23:15 +0000

    Hamming

---
+++ hamming/Hamming.fs
@@ -0,0 +1,7 @@
+﻿module Hamming
+open System.Collections.Generic
+
+let distance (strand1: string) (strand2: string): int option=
+  match Seq.length strand1 = Seq.length strand2 with
+    | true -> (Seq.toList strand1, Seq.toList strand2) ||> List.zip |> List.filter (fun (x, y) -> x <> y) |> List.length |> Some
+    | _ -> None
