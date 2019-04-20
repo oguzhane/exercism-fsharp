1cd913b051b31d8858bf1cd3534c643e47d22f48
Oguzhan Ergin <oguzhanergin13@gmail.com>
2019-04-20 15:55:36 +0100

    Pangram

---
+++ pangram/Pangram.fs
@@ -0,0 +1,14 @@
+﻿module Pangram
+open System
+
+// inclusive 'betwwen' operator
+let (>=<) x (min, max) =
+    (x >= min) && (x <= max)
+    
+let let2nat (letter : char) = int letter - int 'a'
+
+let isPangram (input: string): bool = 
+  match Seq.length input with
+  | i when i > 25 -> input.ToLower() |> Seq.distinct |> Seq.filter (fun c -> let2nat c >=< (0, 25)) |> Seq.length |> (fun len -> len = 26)
+  | i -> false
+  
