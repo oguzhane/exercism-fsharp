d3968fb273f31d56eecfe4b0dadd102d295e951e
Oguzhan Ergin <oguzhanergin13@gmail.com>
2019-08-05 02:11:44 +0100

    simplify & use partial active patterns

--- bob/Bob.fs
+++ bob/Bob.fs
@@ -1,28 +1,21 @@
 ﻿module Bob
 
+open System
 
-let isQuestion (input:string) : bool = 
-  input.Length > 0 && 
-  input.[input.Length - 1] = '?' 
+let (|Question|_|) (input:string) =
+    if input.EndsWith("?") then Some Question else None
 
-let isYell (input:string): bool =
-  let inputSet = set (input)
-  Set.intersect (inputSet) (set ['a'..'z']) |> Set.count = 0 && Set.intersect (inputSet) (set ['A'..'Z']) |> Set.count > 0
+let (|Yell|_|) (input:string) =
+    if input |> Seq.exists Char.IsLetter && input |> Seq.exists Char.IsLower |> not then Some Yell else None
 
-let isYellWithQuestion (input:string): bool = 
-  isQuestion input && isYell input
+let (|NoSaying|_|) (input:string) =
+    if String.IsNullOrWhiteSpace(input) then Some NoSaying else None
 
-let noSaying (input:string): bool =
-  let inputSet = set (input)
-  Set.intersect (inputSet) (set ['a'..'z']) |> Set.count = 0 
-  && Set.intersect (inputSet) (set ['A'..'Z']) |> Set.count = 0
-  && Set.intersect (inputSet) (set ['0'..'9']) |> Set.count = 0
- 
 let response (input: string): string = 
   match input.Trim() with
-  | s when isYellWithQuestion s  -> "Calm down, I know what I'm doing!"
-  | s when isYell s -> "Whoa, chill out!" 
-  | s when isQuestion s  -> "Sure." 
-  | s when noSaying s -> "Fine. Be that way!" 
+  | Question & Yell -> "Calm down, I know what I'm doing!"
+  | Yell -> "Whoa, chill out!" 
+  | Question  -> "Sure." 
+  | NoSaying -> "Fine. Be that way!" 
   | _ -> "Whatever."
 
