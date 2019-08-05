46d541e58786a0dcda0a60ae3b2f42a8adc472cf
Oguzhan Ergin <oguzhanergin13@gmail.com>
2019-08-05 15:57:51 +0100

    add Proverb

---
+++ proverb/Proverb.fs
@@ -0,0 +1,10 @@
+﻿module Proverb
+
+let rec reciteBody (input:string list): string list =
+  match input with
+    | [_] | [] -> []
+    | first :: rest -> [sprintf "For want of a %s the %s was lost." first rest.Head] @ reciteBody rest
+  
+
+let recite (input: string list): string list = 
+  reciteBody input @ (if input.Length = 0 then [] else [sprintf "And all for the want of a %s." input.Head]) 
