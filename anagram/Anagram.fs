b4a6ac029690340c9864e2ad9565412c477f74ed
Oguzhan Ergin <oguzhanergin13@gmail.com>
2020-02-01 14:20:28 +0000

    updates

---
+++ anagram/Anagram.fs
@@ -0,0 +1,13 @@
+﻿module Anagram
+
+let detectAnagram (word:string, target:string) = 
+  word = word.ToLower()
+
+
+
+let findAnagrams sources target = 
+  let targetSorted = (Seq.sort target).ToString().ToLower()
+  sources |> List.filter (fun (x:string) -> 
+    let word = x.ToLower(); 
+    word = targetSorted && 
+    )
